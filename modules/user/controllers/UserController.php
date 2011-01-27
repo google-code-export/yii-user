f9475bb3ed4bae117e839a05d901c92128c416f5
vitaliy.step@gmail.com <vitaliy.step@gmail.com@2f1f60e2-f16f-11de-acd2-8fd6687fc178>
2011-01-27 12:36:30 +0000

    Added merging of controller filters with parent::filters() to provide more flexibility by configuring it via parent 'Controller' class

--- modules/user/controllers/UserController.php
+++ modules/user/controllers/UserController.php
@@ -10,12 +10,13 @@ class UserController extends Controller
 	/**
 	 * @return array action filters
 	 */
-	public function filters()
+  	public function filters()
 	{
-		return array(
+		return CMap::mergeArray(parent::filters(),array(
 			'accessControl', // perform access control for CRUD operations
-		);
+		));                          
 	}
+	
 	/**
 	 * Specifies the access control rules.
 	 * This method is used by the 'accessControl' filter.
