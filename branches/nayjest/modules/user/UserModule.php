a9ac1ab37c92e5d49119fe0edb64cba27155c803
vitaliy.step@gmail.com <vitaliy.step@gmail.com@2f1f60e2-f16f-11de-acd2-8fd6687fc178>
2011-01-31 16:09:28 +0000

    Probably fixed issue #47. Also this changes needed for widgets realization. (controller->module changed to getModule('user') is static UserModule methods)

--- branches/nayjest/modules/user/UserModule.php
+++ branches/nayjest/modules/user/UserModule.php
@@ -135,7 +135,7 @@ class UserModule extends CWebModule
 	 * @return hash string.
 	 */
 	public static function encrypting($string="") {
-		$hash = Yii::app()->controller->module->hash;
+		$hash = Yii::app()->getModule('user')->hash;
 		if ($hash=="md5")
 			return md5($string);
 		if ($hash=="sha1")
@@ -151,8 +151,8 @@ class UserModule extends CWebModule
 	public static function doCaptcha($place = '') {
 		if(!extension_loaded('gd'))
 			return false;
-		if (in_array($place, Yii::app()->controller->module->captcha))
-			return Yii::app()->controller->module->captcha[$place];
+		if (in_array($place, Yii::app()->getModule('user')->captcha))
+			return Yii::app()->getModule('user')->captcha[$place];
 		return false;
 	}
 	
