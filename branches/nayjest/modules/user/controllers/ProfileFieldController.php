be98ba4433c81e59749ef73c0113daa9dc89b969
vitaliy.step@gmail.com <vitaliy.step@gmail.com@2f1f60e2-f16f-11de-acd2-8fd6687fc178>
2011-02-02 16:43:15 +0000

    Removed redundant accessRules from ProfileFieldController

--- branches/nayjest/modules/user/controllers/ProfileFieldController.php
+++ branches/nayjest/modules/user/controllers/ProfileFieldController.php
@@ -28,10 +28,6 @@ class ProfileFieldController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('*'),
-				'users'=>array('*'),
-			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('create','update','view','admin','delete'),
 				'users'=>UserModule::getAdmins(),
