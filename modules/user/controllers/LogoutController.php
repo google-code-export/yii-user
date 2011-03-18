a8efa321e48bd7d9459831466ef77cbae60b9d7f
vitaliy.step@gmail.com <vitaliy.step@gmail.com@2f1f60e2-f16f-11de-acd2-8fd6687fc178>
2011-03-18 16:09:11 +0000

    Added possibility to specify returnUrl as get parameter in logout action

--- modules/user/controllers/LogoutController.php
+++ modules/user/controllers/LogoutController.php
@@ -3,14 +3,14 @@
 class LogoutController extends Controller
 {
 	public $defaultAction = 'logout';
-	
+
 	/**
-	 * Logout the current user and redirect to returnLogoutUrl.
-	 */
-	public function actionLogout()
+	* Logout the current user and redirect to returnLogoutUrl.
+	*/
+	public function actionLogout($redirect = null)
 	{
 		Yii::app()->user->logout();
-		$this->redirect(Yii::app()->controller->module->returnLogoutUrl);
+		$this->redirect($redirect? urldecode($redirect) : Yii::app()->controller->module->returnLogoutUrl);
 	}
 
 }
